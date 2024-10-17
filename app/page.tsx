4ff51f557d56430255d56712931317d9595e1994
Avenir Krinitsin <krinitsin.avenir@gmail.com>
2024-10-17 14:12:29 +0500

    LinkFix-17-10

--- app/page.tsx
+++ app/page.tsx
@@ -29,12 +29,12 @@ export default function Home() {
                 992: { slidesPerView: 3 },
               }}
             >
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-1.jpg' className='card-image'></img></a></SwiperSlide>
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-2.jpg' className='card-image'></img></a></SwiperSlide>
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-3.jpg' className='card-image'></img></a></SwiperSlide>
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-4.jpg' className='card-image'></img></a></SwiperSlide>
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-5.jpg' className='card-image'></img></a></SwiperSlide>
-              <SwiperSlide><a href='/'><img src='/image/announce-pic-6.jpg' className='card-image'></img></a></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-1.jpg' className='card-image'></img></Link></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-2.jpg' className='card-image'></img></Link></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-3.jpg' className='card-image'></img></Link></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-4.jpg' className='card-image'></img></Link></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-5.jpg' className='card-image'></img></Link></SwiperSlide>
+              <SwiperSlide><Link href='/'><img src='/image/announce-pic-6.jpg' className='card-image'></img></Link></SwiperSlide>
             </Swiper>
           </div>
         </section>
@@ -44,27 +44,27 @@ export default function Home() {
           <ul>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-1.png' alt='/'></img>Иконы рока. Как всё начиналось: Крис Корнел</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-1.png' alt='/'></img>Иконы рока. Как всё начиналось: Крис Корнел</Link></p>
             </li>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-2.png' alt='/'></img>«Русская Медиагруппа» выступила инфопартнёром мачта Лиги легенд «Зенит» - ЦСКА</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-2.png' alt='/'></img>«Русская Медиагруппа» выступила инфопартнёром мачта Лиги легенд «Зенит» - ЦСКА</Link></p>
             </li>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-3.png' alt='/'></img>Рок и автомобили: культовые машины рок-звезд</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-3.png' alt='/'></img>Рок и автомобили: культовые машины рок-звезд</Link></p>
             </li>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-4.png' alt='/'></img>21 сентября на «Moscow Raceway» состоится финал кольцевых автогонок суперкаров</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-4.png' alt='/'></img>21 сентября на «Moscow Raceway» состоится финал кольцевых автогонок суперкаров</Link></p>
             </li>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-5.png' alt='/'></img>История одного хита: Highway to Hell (AC/DC)</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-5.png' alt='/'></img>История одного хита: Highway to Hell (AC/DC)</Link></p>
             </li>
             <li>
               <p className='home-news__data'>2 октября</p>
-              <p className='home-news__link'><a href='/'><img src='/image/news-pic-6.png' alt='/'></img>Рокеры, чья дружба проверена временем</a></p>
+              <p className='home-news__link'><Link href='/'><img src='/image/news-pic-6.png' alt='/'></img>Рокеры, чья дружба проверена временем</Link></p>
             </li>
           </ul>
         </section>
@@ -75,17 +75,17 @@ export default function Home() {
             <p className='home-about__txt'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ex impedit cum accusantium ipsum dignissimos velit! Doloribus esse at, expedita consectetur odit corrupti dolorum. Repellendus amet quos ducimus expedita deserunt?</p>
             <ul>
               <li>
-                <p className='home__playlist-name'><a><img src="/image/playlist-1.png" className='home__playlist-img'></img>MORRO ROCK</a></p>
+                <p className='home__playlist-name'><Link href="/"><img src="/image/playlist-1.png" className='home__playlist-img'></img>MORRO ROCK</Link></p>
                 <img src='/icons/icon-arrow.svg' className='img-button'></img>
                 <p className='home__playlist-time'>4h 36m</p>
               </li>
               <li>
-                <p className='home__playlist-name'><a><img src="/image/playlist-2.png" className='home__playlist-img'></img>PACIFIC DREAMS</a></p>
+                <p className='home__playlist-name'><Link href="/"><img src="/image/playlist-2.png" className='home__playlist-img'></img>PACIFIC DREAMS</Link></p>
                 <img src='/icons/icon-arrow.svg' className='img-button'></img>
                 <p className='home__playlist-time'>4h 36m</p>
               </li>
               <li>
-                <p className='home__playlist-name'><a><img src="/image/playlist-3.png" className='home__playlist-img'></img>NIGHT FM</a></p>
+                <p className='home__playlist-name'><Link href="/"><img src="/image/playlist-3.png" className='home__playlist-img'></img>NIGHT FM</Link></p>
                 <img src='/icons/icon-arrow.svg' className='img-button'></img>
                 <p className='home__playlist-time'>4h 36m</p>
               </li>
