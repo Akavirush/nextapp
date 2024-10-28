6aecc5bbc9079b9775ff6124eb891386915428f2
User User <user@vmware.localdomain>
2024-10-28 12:47:11 +0300

    ArticleFixed-28-10

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 
 async function getData(id) {
-    const response = await fetch('http://localhost:3000/api/article/' + id);
+    const response = await fetch(`http://localhost:3000/api/article/${id}`);
     const result = await response.json();
     return result;
 }
 
 const Post = async ({ params: {id} }) => {
-    const post = await getData();
+    const article = await getData(id);
     return (
         <main>
             <div className="container">
                 <div className="article-info">
-                    <h2>{id} Заголовок {post.title}</h2>
-                    <h3>Описание {post.body}</h3>
+                    <h2>{id} Заголовок {article.title}</h2>
+                    <h3>Описание {article.body}</h3>
                 </div>
             </div>
         </main>
